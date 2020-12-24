d3695aa5f6a6894b6e789e8481d3c9a98728edb2
keanu-natchev <keanu.natchev@mail.mcgill.ca>
2020-12-24 18:07:16 -0500

    Created dao package.

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Event.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.eventregistration.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Event {
+
+	public void setName(String name) {
+	}
+
+	public void setDate(Date date) {
+	}
+
+	public void setStartTime(Time startTime) {
+	}
+
+	public void setEndTime(Time endTime) {
+	}
+    
+}
---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Person.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse321.eventregistration.model;
+
+public class Person {
+
+	public void setName(String name) {
+	}
+    
+}
